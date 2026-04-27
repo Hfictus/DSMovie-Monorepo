936191672d61b4ffdf7f7650ff8cab59ee434719
Gerson Klauck <gk120hf@gmail.com>
2026-04-27 11:05:53 -0300

    Root cleanup and frontend .gitignore update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
 
 
 
+import './App.css';
 
-import './App.css'
-
-function App() {
+export default function App() {
   
   return (
-    <h1>Olá, Mundo!</h1>    
+    <h1>Olá, Mundo!</h1>  
   )
 }
 
-export default App
