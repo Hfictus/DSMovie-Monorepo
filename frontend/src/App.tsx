76388bfab2c70f7a664889181ab78c3781507246
Gerson Klauck <gk120hf@gmail.com>
2026-05-01 10:03:30 -0300

    Update App component code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Listing from './routes/Listing';
 import Form from './routes/Form';
@@ -5,6 +6,9 @@ import Header from './components/Header';
 
 
 
+/*Define o Layout Pai (casca da aplicação). 
+  O Header permanece fixo, enquanto o Outlet alterna o conteúdo interno.
+*/
 const RootLayout = () => {
   return (
     <>
@@ -14,25 +18,32 @@ const RootLayout = () => {
   );
 };
 
+/*Criar objeto de roteamento fora do componente,
+  contendo array para definir hierarquia das páginas.
+  Cria a configuração das rotas de forma declarativa.
+  O createBrowserRouter é necessário para habilitar as novas Data APIs do React Router.
+*/
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <RootLayout />,
+    path: "/",  // Rota pai que serve de base para a aplicação
+    element: <RootLayout />, // Componente que provê a estrutura comum (Layout)
     children: [
       {
-        path: "/",
-        element: <Listing />,
+        path: "/",  // Rota raiz (index)
+        element: <Listing />, // Injetado no <Outlet /> quando o caminho for exatamente "/"
       },
       {
-        path: "form/:movieId",
-        element: <Form />,
+        path: "form/:movieId",  // Rota com parâmetro dinâmico
+        element: <Form />, // Injetado no <Outlet /> quando o caminho for "form/algum-id"
       },
     ],
   },
 ]);
 
 function App() {
+  // Provedor que conecta a configuração do roteador à aplicação React
   return <RouterProvider router={router} />;
 }
 
 export default App;
+
