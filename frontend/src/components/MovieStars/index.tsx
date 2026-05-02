0bf74da24f20fa110dea17b78f703e166df555e5
Gerson Klauck <gk120hf@gmail.com>
2026-05-02 12:28:19 -0300

    MovieStars and MovieScore components

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,19 @@
+
+import starFull from '../../assets/star-full.svg';
+import starHalf from '../../assets/star-half.svg';
+import starEmpty from '../../assets/star-empty.svg';
+import './styles.css';
+
+export default function MovieStars() {
+
+    return (
+        <div className="dsmovie-stars-container">
+            <img src={starFull} alt="Estrela amarela toda prenxida" />
+            <img src={starFull} alt="Estrela amarela toda prenxida" />
+            <img src={starFull} alt="Estrela amarela toda prenxida" />
+            <img src={starHalf} alt="Estrela amarela preenxida pela metade" />
+            <img src={starEmpty} alt="Estrela vazia" />
+        </div>
+
+    );
+}
--- frontend/src/routes/Listing/index.tsx
+++ frontend/src/routes/Listing/index.tsx
@@ -1,13 +1,15 @@
 
 
-
 import Pagination from '../../components/Pagination';
+import MovieScore from '../../components/MovieScore';
 
 
 export default function Listing() {
     return(
-        <Pagination />
-        
+        <>
+            <Pagination />
+            <MovieScore />
+        </>
     )
          
 }
