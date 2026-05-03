d14f609a8356a40a7d0ff9f1eb695f39e19fd271
Gerson Klauck <gk120hf@gmail.com>
2026-05-03 13:05:50 -0300

    MovieCard

--- frontend/src/routes/Listing/index.tsx
+++ frontend/src/routes/Listing/index.tsx
@@ -1,15 +1,31 @@
 
 
 import Pagination from '../../components/Pagination';
-import MovieScore from '../../components/MovieScore';
-
+import MovieCard from '../../components/MovieCard';
 
 export default function Listing() {
-    return(
+    return (
         <>
             <Pagination />
-            <MovieScore />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
         </>
     )
-         
 }
