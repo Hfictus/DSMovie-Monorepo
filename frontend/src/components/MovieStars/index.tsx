a022571e0e0882a2d0752e59be6b8977d5715492
Gerson Klauck <gk120hf@gmail.com>
2026-05-17 16:45:34 -0300

    Show score

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -5,15 +5,54 @@ import starHalf from '../../assets/star-half.svg';
 import starEmpty from '../../assets/star-empty.svg';
 import './styles.css';
 
-export default function MovieStars() {
+type Props = {
+    score: number;
+}
+
+type StarProps = {
+    fill: number;
+}
+
+//função auxiliar para construir vetor de números:
+function getFills(score: number) {
+    const fills = [0, 0, 0, 0, 0];
+    const integerPart = Math.floor(score);
+    for(let i = 0; i < integerPart; i++) {
+        fills[i] = 1;
+    }
+    const diff = score - integerPart;
+    if(diff > 0) {
+        fills[integerPart] = 0.5;
+    }
+    return fills;
+}
+
+//Componente não exportado para definir qual estrela será renderizada em cada posição do vetor de getFills:
+function Star({ fill }: StarProps) {
+    if(fill === 0) {
+        return     <img src={starEmpty} alt="Estrela vazia" />
+    }
+    else if(fill === 1) {
+        return <img src={starFull} alt="Estrela amarela toda prenxida" />
+    }
+    else {
+        return <img src={starHalf} alt="Estrela amarela preenxida pela metade" />
+    }
+}
+
+
+export default function MovieStars({ score }: Props) {
+
+    const fills = getFills(score); //Montar vetor de números
 
     return (
+        //Renderizar cada estrela em sua posição de acordo com o if em Star
         <div className="dsmovie-stars-container">
-            <img src={starFull} alt="Estrela amarela toda prenxida" />
-            <img src={starFull} alt="Estrela amarela toda prenxida" />
-            <img src={starFull} alt="Estrela amarela toda prenxida" />
-            <img src={starHalf} alt="Estrela amarela preenxida pela metade" />
-            <img src={starEmpty} alt="Estrela vazia" />
+            <Star fill={fills[0]} />        
+            <Star fill={fills[1]} />
+            <Star fill={fills[2]} />
+            <Star fill={fills[3]} />
+            <Star fill={fills[4]} />
         </div>
 
     );
