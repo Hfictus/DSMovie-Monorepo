ed02e2e34f7b13ac590891483f77ae6243f731d4
Gerson Klauck <gk120hf@gmail.com>
2026-05-17 18:38:40 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,8 +1,15 @@
 
 import arrowIcon from '../../assets/pagination-button.svg';
+import type { MoviePage } from '../../types/movie';
 import './styles.css';
 
-export default function Pagination() {
+type Props = {
+    page: MoviePage;
+    onChange: (newPageNumber: number) => void;
+}
+
+
+export default function Pagination({ page, onChange }: Props) {
 
     return (
 
@@ -10,15 +17,17 @@ export default function Pagination() {
             <div className="dsmovie-pagination-box">
                 <button
                     className="dsmovie-pagination-button"
-                    disabled={true}
+                    disabled={page.first}
+                    onClick={() => onChange(page.number - 1)}
                     aria-label="Página anterior"
                 >
                     <img src={arrowIcon} alt={"Anterior"} />
                 </button>
-                <p>{`${1} de ${3}`}</p>
+                <p>{`${page.number + 1} de ${page.totalPages}`}</p>
                 <button
                     className="dsmovie-pagination-button"
-                    disabled={false}
+                    disabled={page.last}
+                    onClick={() => onChange(page.number + 1)}
                     aria-label="Próxima página"
                 >
                     <img className="dsmovie-flip-horizontal" src={arrowIcon} alt={"página seguinte"} />
