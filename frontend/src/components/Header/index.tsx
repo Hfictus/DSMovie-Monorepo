994bb6481b376fc976aa38c661598ac2dda2c19b
Gerson Klauck <gk120hf@gmail.com>
2026-07-29 12:23:57 -0300

    Fix movie image paths and update project files

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -8,11 +8,11 @@ export default function Header() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior">
+                    <a href="https://github.com/Hfictus/DSMovie-Monorepo">
                         <div className="dsmovie-contact-container">
                             <img src={githubIcon} alt="Github" />
                             <p className="dsmovie-contact-link">
-                                /devsuperior
+                                /Hf-DSMM
                             </p>
                         </div>
                     </a>
